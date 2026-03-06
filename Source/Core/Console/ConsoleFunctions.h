ff4ba94223739fa3446953e46bb38fdcfb956908
dylannws <paulukdylan@gmail.com>
2026-03-05 21:32:55 -0300

    forgot gw1...

--- Source/Core/Console/ConsoleFunctions.h
+++ Source/Core/Console/ConsoleFunctions.h
@@ -1,8 +1,9 @@
 #pragma once
+#define OFFSET_CONSOLEOBJECTUTIL_PROPERTYFUNC CYPRESS_GW_SELECT(0x140390470, 0x1401A89E0)
 
 namespace Cypress
 {
-	void registerConsoleMethod(const char* groupName, const char* name, const char* description, void* pfn = (void*)0x1401A89E0, void* juiceFn = (void*)0);
+	void registerConsoleMethod(const char* groupName, const char* name, const char* description, void* pfn = (void*)OFFSET_CONSOLEOBJECTUTIL_PROPERTYFUNC, void* juiceFn = (void*)0);
 
 #define CYPRESS_REGISTER_CONSOLE_FUNCTION(groupName, name, description, pfn) \
 		registerConsoleMethod(groupName, name, description, pfn, 0)
